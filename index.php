d95e3b5234a85a9a96dea30746c85ec7eddddb7b
ryleybridges <rye.brid@gmail.com>
2019-10-22 21:16:49 +1300

    Sorted out post layout on home page

--- index.php
+++ index.php
@@ -13,22 +13,34 @@
                 <div class="card mt-3">
                     <div class="card-header"><?php the_title(); ?></div>
                     <div class="card-body">
-                        <?php if(is_home()): ?>
-                            <div class="col-md-3"><?php the_post_thumbnail('thumbnail'); ?></div>
-                        <?php else: ?>
-                            <?php the_post_thumbnail('full', ['class' => 'img-fluid']); ?>
-                        <?php endif; ?>
+                        <div class="row">
 
-                        <div>
-                            <?php if(is_home()): ?>
-                                <div class="col-md-9"><?php the_excerpt(); ?></div>
+                            <?php if(has_post_thumbnail()): ?>
+                                <?php if(is_home()): ?>
+                                    <div class="col-md-3"><?php the_post_thumbnail('thumbnail'); ?></div>
+                                <?php else: ?>
+                                    <?php the_post_thumbnail('full', ['class' => 'img-fluid']); ?>
+                                <?php endif; ?>
+                            <?php endif; ?>
+
+                            <?php if(has_post_thumbnail()): ?>
+                                <?php if(is_home()): ?>
+                                    <div class="col-md-9">
+                                        <?php the_excerpt(); ?>
+                                        <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read More</a>
+                                    </div>
+                                <?php else: ?>
+                                    <?php the_content(); ?>
+                                <?php endif; ?>
                             <?php else: ?>
-                                <?php the_content(); ?>
+                                <?php if(is_home()): ?>
+                                    <div class="col">
+                                        <?php the_excerpt(); ?>
+                                        <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read More</a>
+                                    </div>
+                                <?php endif; ?>
                             <?php endif; ?>
                         </div>
-                        <?php if(!is_single()): ?>
-                            <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read More</a>
-                        <?php endif; ?>
 
                     </div>
                 </div>
